d99caf781fafba1bbfd4c23c8a8fa911d3ee2d34
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-10-14 08:42:40 +0100

    Initial Commit

---
+++ Ditdash/DitdashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DitdashApp.swift
+//  Ditdash
+//
+//  Created by Evan Plant on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct DitdashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
