7cacd70427825b818c3c633764ff6924e9e1b87f
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-10-14 18:56:16 +0100

    Start building up MorseView
    working on buttons rn

--- Ditdash/Views/MorseView.swift
+++ Ditdash/Views/MorseView.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct MorseView: View {
     var body: some View {
-        Text("Morse view")
+        Spacer()
+        Text("- - . / . . .") // demo text
+            .font(.title.monospaced())
+            .fontWeight(.bold)
+            .padding()
+            .multilineTextAlignment(.center)
+        Spacer()
+        
+        HStack(spacing: 0) {
+            Button {
+                print("dit")
+            } label: {
+                Circle()
+                    .frame(width: 25)
+                    .foregroundStyle(.foreground)
+                    .padding()
+            }
+            .frame(maxWidth: .infinity, maxHeight: 200)
+            .background(RoundedRectangle(cornerRadius: 15).foregroundStyle(.blue))
+            .padding(7)
+            Button {
+                print("dash")
+            } label: {
+                ZStack {
+                    Capsule()
+                        .frame(width: 50, height: 25)
+                        .foregroundStyle(.foreground)
+                        .padding()
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: 200)
+            .background(RoundedRectangle(cornerRadius: 15).foregroundStyle(.blue))
+            .padding(7)
+        }
+        
+        Spacer()
     }
 }
 
