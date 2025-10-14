a64844a9becd4ef7b483a7a7a513e44568e5644b
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-10-14 19:20:06 +0100

    Update MorseView (buttons + text + etc)

--- Ditdash/Views/MorseView.swift
+++ Ditdash/Views/MorseView.swift
@@ -10,41 +10,121 @@ import SwiftUI
 struct MorseView: View {
     var body: some View {
         Spacer()
+        
         Text("- - . / . . .") // demo text
             .font(.title.monospaced())
             .fontWeight(.bold)
             .padding()
             .multilineTextAlignment(.center)
+        
         Spacer()
         
-        HStack(spacing: 0) {
-            Button {
-                print("dit")
-            } label: {
-                Circle()
-                    .frame(width: 25)
-                    .foregroundStyle(.foreground)
-                    .padding()
-            }
-            .frame(maxWidth: .infinity, maxHeight: 200)
-            .background(RoundedRectangle(cornerRadius: 15).foregroundStyle(.blue))
-            .padding(7)
-            Button {
-                print("dash")
-            } label: {
-                ZStack {
-                    Capsule()
-                        .frame(width: 50, height: 25)
-                        .foregroundStyle(.foreground)
-                        .padding()
+        Text("example text")
+            .font(.headline.monospaced())
+            .padding()
+            .multilineTextAlignment(.center)
+        
+        Spacer()
+        
+        VStack {
+            HStack(spacing: 10) {
+                Button {
+                    print("dit")
+                } label: {
+                    ZStack {
+                        Circle()
+                            .frame(width: 25)
+                            .foregroundStyle(.foreground)
+                            .padding()
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundStyle(.secondary)
+                    }
                 }
+                .frame(maxWidth: .infinity, maxHeight: 200)
+                
+                Button {
+                    print("dash")
+                } label: {
+                    ZStack {
+                        Capsule()
+                            .frame(width: 75, height: 25)
+                            .foregroundStyle(.foreground)
+                            .padding()
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: 200)
             }
-            .frame(maxWidth: .infinity, maxHeight: 200)
-            .background(RoundedRectangle(cornerRadius: 15).foregroundStyle(.blue))
-            .padding(7)
+            
+            HStack(spacing: 10) {
+                Button {
+                    print("clear")
+                } label: {
+                    ZStack {
+                        Label("Clear", systemImage: "xmark")
+                            .foregroundStyle(.foreground)
+                            .labelStyle(.iconOnly)
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+                .frame(maxWidth: 50, maxHeight: 50)
+                
+                Button {
+                    print("space")
+                } label: {
+                    ZStack {
+                        Label("Space", systemImage: "space")
+                            .foregroundStyle(.foreground)
+                            .labelStyle(.iconOnly)
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: 50)
+                
+                Button {
+                    print("next char")
+                } label: {
+                    ZStack {
+                        Label("Next Char", systemImage: "line.diagonal")
+                            .foregroundStyle(.foreground)
+                            .labelStyle(.iconOnly)
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: 50)
+                
+                Button {
+                    print("delete")
+                } label: {
+                    ZStack {
+                        Label("Delete", systemImage: "delete.left")
+                            .foregroundStyle(.foreground)
+                            .labelStyle(.iconOnly)
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: 50)
+                
+                Button {
+                    print("lock buttons")
+                } label: {
+                    ZStack {
+                        Label("Lock", systemImage: "lock")
+                            .foregroundStyle(.foreground)
+                            .labelStyle(.iconOnly)
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+                .frame(maxWidth: 50, maxHeight: 50)
+            }
         }
-        
-        Spacer()
+        .padding()
     }
 }
 
