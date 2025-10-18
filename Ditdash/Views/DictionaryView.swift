c72ab91557fe018329c4be5edf3fba406eaa80f9
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-10-18 17:23:33 +0100

    Start work on dictionary view

--- Ditdash/Views/DictionaryView.swift
+++ Ditdash/Views/DictionaryView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct DictionaryView: View {
+    // i know theres better ways to do this, idrc though
+    let textChars = [
+        "a", "b", "c",
+        "d", "e", "f",
+        "g", "h", "i",
+        "j", "k", "l",
+        "m", "n", "o",
+        "p", "q", "r",
+        "s", "t", "u",
+        "v", "w", "x",
+        "y", "z", // start numbers
+        "0", "1", "2",
+        "3", "4", "5",
+        "6", "7", "8",
+        "9", // start symbols
+        ",", ".", "?",
+        "\"", ":", "'",
+        "-", "/", "(",
+        ")", "!", "@"
+    ]
+    let morseChars = [
+        ".-", "-...", "-.-.", // a b c
+        "-..", ".", "..-.", // d e f
+        "--.", "....", "..", // g h i
+        ".---", "-.-", ".-..", // j k l
+        "--", "-.", "---", // m n o
+        ".--.", "--.-", ".-.", // p q r
+        "...", "-", "..-", // s t u
+        "...-", ".--", "-..-", // v w x
+        "-.--", "--..", // y z, start numbers
+        "-----", ".----", "..---", // 0, 1, 2
+        "...--", "....-", ".....", // 3, 4, 5
+        "-....", "--...", "---..", // 6, 7, 8
+        "----.", // 9, start symbols
+        "--..--", ".-.-.-", "..--..", // , . ?
+        ".-..-.", "---...", ".----.", // " : '
+        "-....-", "-..-.", "-.--.", // - / (
+        "-.--.-", "-.-.--", ".--.-." // ) !
+    ]
+    
     var body: some View {
-        Text("Dictionary view")
+        List {
+            ForEach(textChars.indices) { char in
+                HStack {
+                    Text(textChars[char])
+                }
+            }
+        }
     }
 }
 
