71196c588bdc9860d2e5c012fcc54706c45b584a
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-10-19 21:08:55 +0100

    Start work on fruitview

---
+++ Ditdash/Views/Learn/Words/LearnFruitView.swift
@@ -0,0 +1,21 @@
+//
+//  LearnFruitView.swift
+//  Ditdash
+//
+//  Created by Evan Plant on 19/10/2025.
+//
+
+import SwiftUI
+
+struct LearnFruitView: View {
+    let fruits = [
+        "apple", "banana", "cherry"
+    ]
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearnFruitView()
+}
