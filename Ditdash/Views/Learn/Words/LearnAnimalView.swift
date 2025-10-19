d64d1a88e8973dea3074ed65c4d24f4f5d65fa86
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-10-19 21:21:36 +0100

    Make animalview

---
+++ Ditdash/Views/Learn/Words/LearnAnimalView.swift
@@ -0,0 +1,19 @@
+//
+//  LearnAnimalView.swift
+//  Ditdash
+//
+//  Created by Evan Plant on 19/10/2025.
+//
+
+import SwiftUI
+
+struct LearnAnimalView: View {
+    let animals = []
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearnAnimalView()
+}
